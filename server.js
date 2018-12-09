360ea933e3c474fa4d0ba2c6da4ab677b9ca8a03
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-12-08 17:48:59 -0800

    checking authentication

--- server.js
+++ server.js
@@ -23,6 +23,20 @@ app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
 
+const checkAuth = (req, res, next) => {
+    console.log("Checking authentication");
+    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+        req.user = null;
+    } else {
+        const token = req.cookies.nToken;
+        const decodedToken = jwt.decode(token, { complete: true }) || {};
+        req.user = decodedToken.payload;
+    }
+
+    next();
+};
+app.use(checkAuth);
+
 // Use Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
