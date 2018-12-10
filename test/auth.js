b51a5e61e6aef2fc27691d5503a8643d7afaa401
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-12-10 14:41:49 -0800

    first auth test

---
+++ test/auth.js
@@ -0,0 +1,19 @@
+var chai = require("chai");
+var chaiHttp = require("chai-http");
+var server = require("../server.js");
+var should = chai.should();
+chai.use(chaiHttp);
+
+var agent = chai.request.agent(server);
+
+var User = require("../models/user");
+
+describe("User", function () {
+    // TESTS WILL GO HERE.
+    it("should not be able to login if they have not registered", done => {
+        agent.post("/login", { email: "wrong@wrong.com", password: "nope" }).end(function (err, res) {
+            res.status.should.be.equal(401);
+            done();
+        });
+    });
+});
